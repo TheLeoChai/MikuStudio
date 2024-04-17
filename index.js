9e68a485e86f705a5365ad0f9ed1cbc0a1339f93
Tim Yang <thryang@outlook.com>
2024-04-17 11:24:15 -0400

    Added Product Offerings

---
+++ index.js
@@ -0,0 +1,33 @@
+$('#offer-portrait').hover(
+    function() {
+        $('#img-portrait').animate({opacity: 0.25});
+    }, function() {
+        $('#img-portrait').animate({opacity: 0});
+    }
+);
+
+$('#offer-event').hover(
+    function() {
+        $('#img-event').animate({opacity: 0.25});
+    }, function() {
+        $('#img-event').animate({opacity: 0});
+    }
+);
+
+$('#offer-wedding').hover(
+    function() {
+        $('#img-wedding').animate({opacity: 0.25});
+    }, function() {
+        $('#img-wedding').animate({opacity: 0});
+    }
+);
+
+$('#offer-commercial').hover(
+    function() {
+        $('#img-commercial').animate({opacity: 0.25});
+    }, function() {
+        $('#img-commercial').animate({opacity: 0});
+    }
+);
+
+
