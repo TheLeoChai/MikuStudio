0ae61a65414d34048da658de30438ec4fef7ad0e
TheLeoChai <43290645+TheLeoChai@users.noreply.github.com>
2024-05-03 02:46:12 -0400

    Update content-initiate.js

--- gallery/content-initiate.js
+++ gallery/content-initiate.js
@@ -2,8 +2,8 @@
 let names = ["DSC_2618", "DSC_3160", "DSC_3359", "DSC_4486_square"]
 
 function loadDesc() {
+    var slideName = $(".swiper-slide-visible").data("name");
     $(window).on("load", function(){
-        var slideName = $(".swiper-slide-visible").data("name");
         console.log("switching to " + slideName + "-desc");
         $(".back").removeClass("back").addClass("gone");
         $("#" + slideName + "-desc").addClass("back").removeClass("gone");
