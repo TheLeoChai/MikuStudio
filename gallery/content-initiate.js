716c9cd01f248f39e8b8297f82af1d87acbbb42f
TheLeoChai <43290645+TheLeoChai@users.noreply.github.com>
2024-05-03 00:44:35 -0400

    tried to fix description loading problem again

--- gallery/content-initiate.js
+++ gallery/content-initiate.js
@@ -1,13 +1,20 @@
 
 let names = ["DSC_2618", "DSC_3160", "DSC_3359", "DSC_4486_square"]
 
-function switchDesc() {
-    var slideName = $(".swiper-slide-visible").data("name");
-    $("#" + slideName + "-desc").ready(function(){
+function loadDesc() {
+    $(window).on("load", function(){
         var slideName = $(".swiper-slide-visible").data("name");
         console.log("switching to " + slideName + "-desc");
         $(".back").removeClass("back").addClass("gone");
         $("#" + slideName + "-desc").addClass("back").removeClass("gone");
         console.log($("#" + slideName + "-desc").html());
     });
+}
+
+function switchDesc() {
+    var slideName = $(".swiper-slide-visible").data("name");
+    console.log("switching to " + slideName + "-desc");
+    $(".back").removeClass("back").addClass("gone");
+    $("#" + slideName + "-desc").addClass("back").removeClass("gone");
+    console.log($("#" + slideName + "-desc").html());
 }
