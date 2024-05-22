a68b424fb6d27c5e95543e31c651f52ad0ab3d1b
Tim Yang <thryang@outlook.com>
2024-05-22 11:14:45 -0400

    Modified Product Preview
    Fixed bug to prevent unintended switching between background images. Added description words for each product.

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 $('#offer-portrait').hover(
     function() {
-        $('#img-portrait').animate({opacity: 0.25});
+        $('#img-portrait').stop(true, true).animate({opacity: 0.25});
+        $('#offer-text').text('');
     }, function() {
-        $('#img-portrait').animate({opacity: 0});
+        $('#img-portrait').stop(true, true).animate({opacity: 0});
+        $('#offer-text').text('');
     }
 );
 
 $('#offer-event').hover(
     function() {
-        $('#img-event').animate({opacity: 0.25});
+        $('#img-event').stop(true, true).animate({opacity: 0.25});
     }, function() {
-        $('#img-event').animate({opacity: 0});
+        $('#img-event').stop(true, true).animate({opacity: 0});
     }
 );
 
 $('#offer-wedding').hover(
     function() {
-        $('#img-wedding').animate({opacity: 0.25});
+        $('#img-wedding').stop(true, true).animate({opacity: 0.25});
     }, function() {
-        $('#img-wedding').animate({opacity: 0});
+        $('#img-wedding').stop(true, true).animate({opacity: 0});
     }
 );
 
 $('#offer-commercial').hover(
     function() {
-        $('#img-commercial').animate({opacity: 0.25});
+        $('#img-commercial').stop(true, true).animate({opacity: 0.25});
     }, function() {
-        $('#img-commercial').animate({opacity: 0});
+        $('#img-commercial').stop(true, true).animate({opacity: 0});
     }
 );
 
